7dece7616543c2b191711191a18f61a520f79282
menachem <AM0556727820@GMAIL.COM>
2024-10-07 10:31:34 +0300

    add and connect empty route files

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
 dotenv.config();
-
 const port = process.env.PORT || 1415;
+
+app.use(express.json());
+app.use("/auth", require("./routes/authRoute"));
+app.use("/user", require("./routes/userRoute"));
+app.use("/greenEye", require("./routes/greenEyeRoute"));
+
 app.listen(port, () => console.log(`server up and runing on port ${port}`));
