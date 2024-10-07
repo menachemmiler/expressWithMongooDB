d52be4d2c0b39623c65573858859a4edcbf78a6b
menachem <AM0556727820@GMAIL.COM>
2024-10-07 10:16:03 +0300

    listen to port & initial PORT in .env

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("bla bla!")
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 1415;
+app.listen(port, () => console.log(`server up and runing on port ${port}`));
